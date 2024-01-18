8a473333839639e3a1fd70953fb9c6f63471a632
adi bangkit <adibangkai@gmail.com>
2024-01-18 11:39:09 +0700

    remove button added. does not have function

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ console.log("connect3");
 
 const addForm = document.querySelector(".addForm");
 const inventoryTable = document.getElementById("inventoryTable");
+
 addForm.addEventListener("submit", (e) => {
   const item = document.getElementById("item").value;
   const amount = document.getElementById("amount").value;
@@ -16,8 +17,10 @@ const addData = (item, amount) => {
   // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
   var cell1 = row.insertCell(0);
   var cell2 = row.insertCell(1);
+  var cell3 = row.insertCell(2);
 
   // Add some text to the new cells:
   cell1.innerHTML = item;
   cell2.innerHTML = amount;
+  cell3.innerHTML = `<button class="trash">🗑️</button>  `;
 };

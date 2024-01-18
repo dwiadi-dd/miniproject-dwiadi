9248b483fba326b2719133dfaf31735e96438ca5
adi bangkit <adibangkai@gmail.com>
2024-01-18 23:10:25 +0700

    add comments in index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-console.log("connect3");
-
 const addForm = document.querySelector(".addForm");
 const inventoryTable = document.getElementById("inventoryTable");
 const item = document.getElementById("item");
@@ -10,8 +8,37 @@ const totalPrice = document.getElementById("totalPrice");
 let num = 0;
 let totalPriceResult = 0;
 
+// add data to table
+const addData = (item, amount) => {
+  const row = inventoryTable.insertRow(-1);
+
+  // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+  let cell1 = row.insertCell(0);
+  let cell2 = row.insertCell(1);
+  let cell3 = row.insertCell(2);
+  let cell4 = row.insertCell(3);
+
+  // total price math
+  const price = randomPrice();
+  totalPriceResult += price * amount;
+
+  // Add some text to the new cells:
+  cell1.innerHTML = item;
+  cell2.innerHTML = amount;
+  cell3.innerHTML = price;
+  cell2.classList.add("amount");
+  cell3.classList.add("price");
+  cell4.innerHTML = `<button class="trash">X</button>  `;
+
+  //show totalprice to page
+  totalPrice.innerHTML = rupiah(totalPriceResult);
+};
+
+//submit function
 addForm.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  // pagination not implemented yet, so i add limit
   if (num >= 5) {
     window.alert(
       "you already reach the limit, remove item if you want to add other items"
@@ -25,31 +52,10 @@ addForm.addEventListener("submit", (e) => {
   num++; //item counter for limit
 });
 
+//reset form function
 const resetForm = () => {
   item.value = "";
   amount.value = "";
-};
-
-const addData = (item, amount) => {
-  const row = inventoryTable.insertRow(-1);
-  // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
-  const price = randomPrice();
-  let cell1 = row.insertCell(0);
-  let cell2 = row.insertCell(1);
-  let cell3 = row.insertCell(2);
-  let cell4 = row.insertCell(3);
-
-  totalPriceResult += price * amount;
-
-  // Add some text to the new cells:
-  cell1.innerHTML = item;
-  cell2.innerHTML = amount;
-  cell3.innerHTML = price;
-  cell2.classList.add("amount");
-  cell3.classList.add("price");
-  cell4.innerHTML = `<button class="trash">X</button>  `;
-
-  totalPrice.innerHTML = rupiah(totalPriceResult);
 };
 
 // delete list item
@@ -58,9 +64,11 @@ inventoryTable.addEventListener("click", (e) => {
     const main = e.target.parentElement.parentElement;
     const price = main.querySelector(".price").innerHTML;
     const amount = main.querySelector(".amount").innerHTML;
+
     totalPriceResult -= price * amount;
     e.target.parentElement.parentElement.remove();
     totalPrice.innerHTML = rupiah(totalPriceResult);
+    num--;
   }
 });
 
@@ -72,6 +80,7 @@ const rupiah = (number) => {
   }).format(number);
 };
 
+// random price generator
 const randomPrice = () => {
   return Math.floor(1 + Math.random() * 10) * 1000;
 };
